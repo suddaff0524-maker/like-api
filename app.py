6d9f38da9dbef8d65468bfee273127efc8638133
suddaff0524-maker <suddaff0524@gmail.com>
2026-07-06 00:23:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def get_headers(token):
             "Expect": "100-continue",
             "X-Unity-Version": "2018.4.11f1",
             "X-GA": "v1 1",
-            "ReleaseVersion": "OB53",
+            "ReleaseVersion": "OB54",
         }
 
 def encrypt_message(data):
