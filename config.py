167a64e60a8781920741490366ea31d4d3dcc059
suddaff0524-maker <suddaff0524@gmail.com>
2026-07-06 01:17:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 
 
-URLS_LIKE = {"IND":"https://client.ind.freefiremobile.com/LikeProfile",
+URLS_LIKE = {"SG":"https://client.ind.freefiremobile.com/LikeProfile",
             "BR":"https://client.us.freefiremobile.com/LikeProfile",
             "US":"https://client.us.freefiremobile.com/LikeProfile",
             "SAC":"https://client.us.freefiremobile.com/LikeProfile",
             "NA":"https://client.us.freefiremobile.com/LikeProfile"}
 
 
-URLS_INFO = {"IND":"https://client.ind.freefiremobile.com/GetPlayerPersonalShow",
+URLS_INFO = {"SG":"https://client.ind.freefiremobile.com/GetPlayerPersonalShow",
             "BR":"https://client.us.freefiremobile.com/GetPlayerPersonalShow",
             "US":"https://client.us.freefiremobile.com/GetPlayerPersonalShow",
             "SAC":"https://client.us.freefiremobile.com/GetPlayerPersonalShow",
@@ -15,7 +15,7 @@ URLS_INFO = {"IND":"https://client.ind.freefiremobile.com/GetPlayerPersonalShow"
 
 
 
-FILES = {"IND":"token_ind.json",
+FILES = {"SG":"token_sg.json",
              "BR":"token_br.json",
              "US":"token_br.json",
              "SAC":"token_br.json",
